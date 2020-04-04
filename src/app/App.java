e65b9a02c4def65fd83b05b0d7989a33707e5318
AndreaMessina31 <maktub_dharma@hotmail.com>
2020-04-04 12:59:47 -0300

    Modificaciones en websodio

--- src/app/App.java
+++ src/app/App.java
@@ -1,11 +1,16 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
+
+    public static Scanner Teclado = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
 
         Potflix miPotflix = new Potflix();
 
-        System.out.println("Inicializando Catalogo");
+        System.out.println("Inicializando Catalogo....");
 
         miPotflix.inicializarCatalogo();
 
@@ -13,11 +18,46 @@ public class App {
 
         Temporada laMejorTemporada = bb.getTemporada(1);
 
+        Episodio epiDemo;
+
+        int posicion = 0;
+
+        /// En base a un numero de posicion de episodio, obtenerlo de la lista
+        System.out.println("Ingrese la posicion");
+
+        posicion = Teclado.nextInt();
+
+        epiDemo = laMejorTemporada.getEpisodioAtPosicion(posicion);
+
+        epiDemo.reproducir();
+
+        // instanceof: devuelve si la variable es de tipo Clase X
+        // En este caso, devuelve si epiDemo declarado como Episodio apunta a una clase
+        // Websodio
+        if (epiDemo instanceof Websodio) {
+            System.out.println("Estoy reproduciendo un websodio");
+
+            // (clase): Operador casteo.
+            // Casteo lo que hace es acomodar la variable a castear, a una clase especifica
+            // da error si no se puede acomodar. por eso siempre se pregunta antes con un
+            // instance of
+            Websodio webso = (Websodio) epiDemo;
+            webso.enviarAlertaDeQueEstaOnline();
+        } else
+            System.out.println("Estoy reproduciendo un Episodio");
+
         Episodio elTop;
 
         elTop = laMejorTemporada.getEpisodio(3);
 
         elTop.reproducir();
+
+        Episodio otroEpi;
+
+        otroEpi = laMejorTemporada.getEpisodio(1);
+
+        otroEpi.reproducir();
+
     }
 
 }
